28361209ba9c9ec7e342e624d840f86e05aa8a45
Roy A Vanegas <roy@thecodeeducators.com>
2026-03-12 09:15:57 -0400

    Update output folder name

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const htmlCompressor = require(`gulp-htmlmin`);
 let compressHTML = () => {
     return src(`uncompressed-html/*.html`)
         .pipe(htmlCompressor({collapseWhitespace: true}))
-        .pipe(dest(`compressed-html/`));
+        .pipe(dest(`processed-html/`));
 };
 
 let validateHTML = () => {
